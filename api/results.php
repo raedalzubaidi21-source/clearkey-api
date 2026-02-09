36bb46fbaf8e0a08b8c6ca12103c3d890a22f0fe
raedalzubaidi21-source <raedalzubaidi21@gmail.com>
2026-02-09 17:23:21 +0300

    Update results.php

--- api/results.php
+++ api/results.php
@@ -1,23 +1,15 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-
-$keyId = $_GET['keyid'] ?? '';
-$key = $_GET['key'] ?? '';
-
-if (empty($keyId) || empty($key)) {
-echo json_encode(['error' => 'Missing parameters']);
-exit;
-}
-
-$response = [
-'keys' => [
-[
+$keyId = $_GET['keyid'];
+$key = $_GET['key'];
+$jsonData = array(
+'keys' => array(
+array(
 'kty' => 'oct',
-'kid' => base64_encode(hex2bin($keyId)),
-'k' => base64_encode(hex2bin($key))
-]
-]
-];
-
-echo json_encode($response);
+'k' => base64_encode(hex2bin($key)),
+'kid' => base64_encode(hex2bin($keyId))
+)
+)
+);
+echo json_encode($jsonData);
