e4970f0ce70d5cb7789c98ae3a6d1ed5ca91e2ca
raedalzubaidi21-source <raedalzubaidi21@gmail.com>
2026-02-10 00:11:53 +0300

    Update results.php

--- api/results.php
+++ api/results.php
@@ -1,16 +1,21 @@
 <?php
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
 
-$kid = "PUT_BASE64_KID";
-$key = "PUT_BASE64_KEY";
+$keyid = isset($_GET['keyid']) ? $_GET['keyid'] : 'ab081a704aad41829b1123b09b6ecafd';
+$key = isset($_GET['key']) ? $_GET['key'] : 'dd51609092fff15e68b91debf5d591f9';
+
+// تحويل HEX إلى Base64
+function hex2base64($hex) {
+    return base64_encode(pack('H*', $hex));
+}
 
 echo json_encode([
- "keys"=>[
-  [
-   "kty"=>"oct",
-   "kid"=>$kid,
-   "k"=>$key
-  ]
- ]
+    "keys" => [
+        [
+            "kty" => "oct",
+            "kid" => hex2base64($keyid),
+            "k" => hex2base64($key)
+        ]
+    ]
 ]);
+?>
